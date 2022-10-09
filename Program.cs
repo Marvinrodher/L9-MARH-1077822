99b418e460b6df6128e5a5a5a3ddab516a919af0
Alexander <Alexander@DESKTOP-7GV22OO>
2022-10-09 17:09:37 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿namespace Lab9_MARH_1077822
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Automovil
+    {
+        private int modelo;
+        private double precio;
+        private string marca;
+        private bool disponible;
+        private double tipoCambiaDolar;
+        private double descuentoAplicado;
+
+        public Automovil (int Modelo, double Precio, string Marca, bool Disponible, double TipoCambiaDolar, double DescuentoAplicado)
+        {
+            modelo = Modelo;
+            precio = Precio;
+            marca = Marca;
+            disponible = Disponible;
+            tipoCambiaDolar = TipoCambiaDolar;
+            descuentoAplicado = DescuentoAplicado;
+        }
+
+        public void DefinirModelo(int unModelo)
+        {
+            modelo = unModelo;
+        }
+        public void DefinirPrecio(int unPrecio)
+        {
+            precio = unPrecio;
+        }
+        public void DefinirMarca(int unaMarca)
+        {
+            modelo = unaMarca; 
+        }
+        public void DefinirTipoCambio(double unTipoCambio)
+        {
+            tipoCambiaDolar = unTipoCambio;
+        }
+
+        public void CambiarDisponibilidad()
+        {
+            if (disponible == true)
+            {
+                disponible = false;
+            }
+            else
+            {
+                disponible = true;
+            }
+        }
+
+        public string MostrarDisponibilidad()
+        {
+            if (disponible == true)
+            {
+                return "Disponible";
+            }
+            else
+            {
+                return "No se encuentra disponible actualmente";
+            }
+        }
+        public string MostrarInformacion()
+        {
+            return "Marca: " + marca + ", precio: " + precio;
+        }
+    }
+}
+
+   
